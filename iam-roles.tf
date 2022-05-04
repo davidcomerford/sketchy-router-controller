0622d04347244c3d6e79e71abe44574fa5f43199
David Comerford <david.comerford@sungardas.com>
2022-05-04 15:10:57 +0100

    Improved the names

--- iam-roles.tf
+++ iam-roles.tf
@@ -1,38 +1,37 @@
 /*
 Control of EC2
 */
-resource "aws_iam_role" "ec2_control" {
+resource "aws_iam_role" "ec2_control_role" {
   name               = "sketchy-router-ec2"
   assume_role_policy = file("iam-trust-policy-apigateway.json")
 }
 
-resource "aws_iam_policy_attachment" "attachment_ec2_control" {
-  name = "attachment_ec2_control_policy"
-  policy_arn = aws_iam_policy.policy_ec2.arn
-  roles = [aws_iam_role.ec2_control.name]
+resource "aws_iam_policy" "ec2_control_policy" {
+  name        = "sketch-router-ec2"
+  policy      = file("iam-policy-ec2.json")
 }
 
-resource "aws_iam_policy" "policy_ec2" {
-  name        = "ec2_control"
-  policy      = file("iam-policy-ec2.json")
+resource "aws_iam_policy_attachment" "ec2_control_attachment" {
+  name = "ec2_control_attachment"
+  roles = [aws_iam_role.ec2_control_role.name]
+  policy_arn = aws_iam_policy.ec2_control_policy.arn
 }
 
 /*
 API Gateway Logging
 */
-resource "aws_iam_role" "role_sketchy_router_logging" {
+resource "aws_iam_role" "logging_role" {
   name               = "sketchy-router-logging"
   assume_role_policy = file("iam-trust-policy-logging.json")
 }
 
-resource "aws_iam_policy" "policy_api_logging" {
-  name        = "test-policy2"
-  description = "A test policy"
+resource "aws_iam_policy" "logging_policy" {
+  name        = "sketchy-router-logging"
   policy      = file("iam-policy-logging.json")
 }
 
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "attachment-logging-role"
-  roles      = [aws_iam_role.role_sketchy_router_logging.name]
-  policy_arn = aws_iam_policy.policy_api_logging.arn
+resource "aws_iam_policy_attachment" "logging_role_attachment" {
+  name       = "logging-role-attachment"
+  roles      = [aws_iam_role.logging_role.name]
+  policy_arn = aws_iam_policy.logging_policy.arn
 }
